0855abb507a5eddfdd7e087ca25c84714f15fb91
Girish Muley <girish.muley007@gmail.com>
2023-12-17 14:42:27 +0530

    Checkout Page and Order API

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -9,7 +9,7 @@ import {
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 const products = [
   {
@@ -59,6 +59,7 @@ export default function Cart() {
 
   return (
     <>
+      {!items.length && <Navigate to="/" replace={true}></Navigate>}
       <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
         <div>
           <div className="border-t border-gray-200 px-4 py-6 sm:px-6">

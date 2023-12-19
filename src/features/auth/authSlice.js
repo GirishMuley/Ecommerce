248301dea7a04ae305613844faebec85c32f60ca
Girish Muley <girish.muley007@gmail.com>
2023-12-19 15:45:28 +0530

    Some clean up

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -44,7 +44,7 @@ export const signOutAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
+export const authSlice = createSlice({
   name: "user",
   initialState,
 
@@ -94,5 +94,5 @@ export const counterSlice = createSlice({
 export const selectLoggedInUser = (state) => state.auth.loggedInUser;
 export const selectError = (state) => state.auth.error;
 
-export const { increment } = counterSlice.actions;
-export default counterSlice.reducer;
+export const { increment } = authSlice.actions;
+export default authSlice.reducer;

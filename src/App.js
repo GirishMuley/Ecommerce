4aaa03f9313568a77928efd0a9e589bdcbab9780
Girish Muley <girish.muley007@gmail.com>
2024-01-04 15:07:44 +0530

    Stripe integration

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import AdminProductFormPage from "./pages/AdminProductFormPage";
 import AdminOrdersPage from "./pages/AdminOrdersPage";
 import { positions, Provider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import StripeCheckout from "./pages/StripeCheckout";
 
 const options = {
   timeout: 5000,
@@ -145,6 +146,15 @@ const router = createBrowserRouter([
       <Protected>
         {" "}
         <UserProfilePage></UserProfilePage>{" "}
+      </Protected>
+    ),
+  },
+  {
+    path: "/stripe-checkout/",
+    element: (
+      <Protected>
+        {" "}
+        <StripeCheckout></StripeCheckout>{" "}
       </Protected>
     ),
   },
